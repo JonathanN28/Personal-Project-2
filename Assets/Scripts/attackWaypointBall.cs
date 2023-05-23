49efb325d2594b8f729e79ac27c9426c6ba99607
JonathanN28 <98481216+JonathanN28@users.noreply.github.com>
2023-05-23 13:21:00 -0400

    ai

---
+++ Assets/Scripts/attackWaypointBall.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class attackWaypointBall : MonoBehaviour
+{
+    public GameObject playerBall;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ()
+    }
+}
